012e4f7d23962855ae925e9ee4a484f1207d586e
Marlene Uwamahoro <uwamahoromarlene@gmail.com>
2017-02-06 10:29:17 +0200

    Answered the haystack exercise

--- algo/haystack.rb
+++ algo/haystack.rb
@@ -3,4 +3,4 @@
 # haystack[1]
 
 haystack = {hay: [:hay, :hay, :hay, {hay: {hay: [:hay, {hay: [:hay, :hay, :needle]}, :hay, :hay, :hay]}}, :hay, :hay]}
-p haystack
+p haystack [:hay][3][:hay][:hay][1][:hay][2]
